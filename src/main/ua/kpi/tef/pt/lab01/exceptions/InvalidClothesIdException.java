2b0e36aac1052b5e77fe608bb770ac0c811e6fdd
Denys <dodenisa@gmail.com>
2020-11-18 19:17:34 +0200

    fixed exceptions

--- src/main/ua/kpi/tef/pt/lab01/exceptions/InvalidClothesIdException.java
+++ src/main/ua/kpi/tef/pt/lab01/exceptions/InvalidClothesIdException.java
@@ -1,6 +1,6 @@
 package ua.kpi.tef.pt.lab01.exceptions;
 
-public class InvalidClothesIdException extends Throwable { // + todo created exception class (12)
+public class InvalidClothesIdException extends RuntimeException { // + todo created exception class (12)
 
     public InvalidClothesIdException(){
         super();
--- src/main/ua/kpi/tef/pt/lab01/exceptions/InvalidClothingSectionException.java
+++ src/main/ua/kpi/tef/pt/lab01/exceptions/InvalidClothingSectionException.java
@@ -1,6 +1,6 @@
 package ua.kpi.tef.pt.lab01.exceptions;
 
-public class InvalidClothingSectionException extends Throwable {
+public class InvalidClothingSectionException extends RuntimeException {
     public InvalidClothingSectionException(){
         super();
     }
