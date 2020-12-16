fd8fea6c268bad33186c0773155a004c80216b45
Denys <dodenisa@gmail.com>
2020-12-16 23:47:23 +0200

    Module availability fix

--- clothes-designer/src/main/java/module-info.java
+++ clothes-designer/src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module ua.kpi.tef.pt.clothesdesigner {
-    exports ua.kpi.tef.pt.lab01.model.parts;
-    exports ua.kpi.tef.pt.lab01.model;
-    exports ua.kpi.tef.pt.lab01.exceptions;
-    exports ua.kpi.tef.pt.lab01.service;
-    exports ua.kpi.tef.pt.lab01.controller;
+    opens ua.kpi.tef.pt.lab01.model.parts;
+    opens ua.kpi.tef.pt.lab01.model;
+    opens ua.kpi.tef.pt.lab01.exceptions;
+    opens ua.kpi.tef.pt.lab01.service;
+    opens ua.kpi.tef.pt.lab01.controller;
     //exports ua.kpi.tef.pt.lab01;
 }
