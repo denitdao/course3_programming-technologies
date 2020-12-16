6d889985ad4b94695b06bfd1463e03a3127a17a2
Denys <dodenisa@gmail.com>
2020-12-16 23:11:24 +0200

    Working maven test config (no cyrillic in path) - integration tests - unit tests - LowerBodyClothesServiceIT completed

--- clothes-designer/src/main/java/module-info.java
+++ clothes-designer/src/main/java/module-info.java
@@ -4,4 +4,5 @@ module ua.kpi.tef.pt.clothesdesigner {
     exports ua.kpi.tef.pt.lab01.exceptions;
     exports ua.kpi.tef.pt.lab01.service;
     exports ua.kpi.tef.pt.lab01.controller;
+    //exports ua.kpi.tef.pt.lab01;
 }
---
+++ clothes-designer/src/test/java/ua/kpi/tef/pt/lab01/service/LowerBodyClothesServiceIT.java
@@ -0,0 +1,154 @@
+package ua.kpi.tef.pt.lab01.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ua.kpi.tef.pt.lab01.model.*;
+import ua.kpi.tef.pt.lab01.model.parts.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class LowerBodyClothesServiceIT {
+
+    String defTitle = "Default Title";
+    Type defType = Type.CHILDREN;
+    Name defName = Name.JEANS;
+    Size defSize = Size.L;
+    Double defPrice = 80.;
+    LeftLeg defLeftLeg = new LeftLeg(Material.COTTON, Color.BLUE, Fit.CLASSIC);
+    RightLeg defRightLeg = new RightLeg(Material.COTTON, Color.BLUE, Fit.CLASSIC);
+    Button defButton = new Button(Material.COTTON, Color.BLUE, 10);
+
+    @Mock
+    LeftLegService leftLegService;
+
+    @Mock
+    RightLegService rightLegService;
+
+    @Mock
+    ButtonService buttonService;
+
+    @InjectMocks
+    LowerBodyClothesService lowerBodyClothesService;
+
+    @Test
+    public void When_EditLeftLeg_Expect_ValueChanged() {
+        LowerBodyClothes lowerBodyClothes = new LowerBodyClothesService(null, null, null)
+                .create(defTitle, defType, defName, defSize, defPrice, defLeftLeg, defRightLeg, defButton); // all data
+        Mockito.doAnswer(invocationOnMock -> {
+            Object[] args = invocationOnMock.getArguments();
+            ((LeftLeg) args[0]).setMaterial(Material.METAL);
+            ((LeftLeg) args[0]).setColor(Color.GREEN);
+            ((LeftLeg) args[0]).setFit(Fit.REGULAR);
+            return null;
+        }).when(leftLegService).edit(defLeftLeg, Material.METAL, Color.GREEN, Fit.REGULAR);
+
+        lowerBodyClothesService.editLeftLeg(lowerBodyClothes, Material.METAL, Color.GREEN, Fit.REGULAR);
+
+        Mockito.verify(leftLegService).edit(defLeftLeg, Material.METAL, Color.GREEN, Fit.REGULAR); // count
+        assertEquals("Left Leg\n" +
+                "\tmaterial: Metal\n" +
+                "\tcolor: Green\n" +
+                "\tfit: Regular",
+                lowerBodyClothes.getPart(0).toString()); // check result
+    }
+
+    @Test
+    public void When_EditLeftLegOnEmpty_Expect_ValueAdded() {
+        LowerBodyClothes lowerBodyClothes = new LowerBodyClothesService(null, null, null)
+                .create(defTitle, defType, defName, defSize, defPrice); // no left leg
+        Mockito.when(leftLegService.create(Material.METAL, Color.GREEN, Fit.REGULAR))
+                .thenReturn(new LeftLeg(Material.METAL, Color.GREEN, Fit.REGULAR));
+
+        lowerBodyClothesService.editLeftLeg(lowerBodyClothes, Material.METAL, Color.GREEN, Fit.REGULAR);
+
+        Mockito.verify(leftLegService).create(Material.METAL, Color.GREEN, Fit.REGULAR); // count
+        assertEquals("Left Leg\n" +
+                        "\tmaterial: Metal\n" +
+                        "\tcolor: Green\n" +
+                        "\tfit: Regular",
+                lowerBodyClothes.getPart(0).toString()); // check result
+    }
+
+    @Test
+    public void When_EditRightLeg_Expect_ValueChanged() {
+        LowerBodyClothes lowerBodyClothes = new LowerBodyClothesService(null, null, null)
+                .create(defTitle, defType, defName, defSize, defPrice, defLeftLeg, defRightLeg, defButton); // all data
+        Mockito.doAnswer(invocationOnMock -> {
+            Object[] args = invocationOnMock.getArguments();
+            ((RightLeg) args[0]).setMaterial(Material.METAL);
+            ((RightLeg) args[0]).setColor(Color.GREEN);
+            ((RightLeg) args[0]).setFit(Fit.REGULAR);
+            return null;
+        }).when(rightLegService).edit(defRightLeg, Material.METAL, Color.GREEN, Fit.REGULAR);
+
+        lowerBodyClothesService.editRightLeg(lowerBodyClothes, Material.METAL, Color.GREEN, Fit.REGULAR);
+
+        Mockito.verify(rightLegService).edit(defRightLeg, Material.METAL, Color.GREEN, Fit.REGULAR); // count
+        assertEquals("Right Leg\n" +
+                        "\tmaterial: Metal\n" +
+                        "\tcolor: Green\n" +
+                        "\tfit: Regular",
+                lowerBodyClothes.getPart(1).toString()); // check result
+    }
+
+    @Test
+    public void When_EditRightLegOnEmpty_Expect_ValueAdded() {
+        LowerBodyClothes lowerBodyClothes = new LowerBodyClothesService(null, null, null)
+                .create(defTitle, defType, defName, defSize, defPrice); // no right leg
+        Mockito.when(rightLegService.create(Material.METAL, Color.GREEN, Fit.REGULAR))
+                .thenReturn(new RightLeg(Material.METAL, Color.GREEN, Fit.REGULAR));
+
+        lowerBodyClothesService.editRightLeg(lowerBodyClothes, Material.METAL, Color.GREEN, Fit.REGULAR);
+
+        Mockito.verify(rightLegService).create(Material.METAL, Color.GREEN, Fit.REGULAR); // count
+        assertEquals("Right Leg\n" +
+                        "\tmaterial: Metal\n" +
+                        "\tcolor: Green\n" +
+                        "\tfit: Regular",
+                lowerBodyClothes.getPart(0).toString()); // check result
+    }
+
+    @Test
+    public void When_EditButton_Expect_ValueChanged() {
+        LowerBodyClothes lowerBodyClothes = new LowerBodyClothesService(null, null, null)
+                .create(defTitle, defType, defName, defSize, defPrice, defLeftLeg, defRightLeg, defButton); // all data
+        Mockito.doAnswer(invocationOnMock -> {
+            Object[] args = invocationOnMock.getArguments();
+            ((Button) args[0]).setMaterial(Material.METAL);
+            ((Button) args[0]).setColor(Color.GREEN);
+            ((Button) args[0]).setAmount(15);
+            return null;
+        }).when(buttonService).edit(defButton, Material.METAL, Color.GREEN, 15);
+
+        lowerBodyClothesService.editButton(lowerBodyClothes, Material.METAL, Color.GREEN, 15);
+
+        Mockito.verify(buttonService).edit(defButton, Material.METAL, Color.GREEN, 15); // count
+        assertEquals("Button\n" +
+                        "\tmaterial: Metal\n" +
+                        "\tcolor: Green\n" +
+                        "\tamount: 15",
+                lowerBodyClothes.getPart(2).toString()); // check result
+    }
+
+    @Test
+    public void When_EditButtonOnEmpty_Expect_ValueAdded() {
+        LowerBodyClothes lowerBodyClothes = new LowerBodyClothesService(null, null, null)
+                .create(defTitle, defType, defName, defSize, defPrice); // no button
+        Mockito.when(buttonService.create(Material.METAL, Color.GREEN, 15))
+                .thenReturn(new Button(Material.METAL, Color.GREEN, 15));
+
+        lowerBodyClothesService.editButton(lowerBodyClothes, Material.METAL, Color.GREEN, 15);
+
+        Mockito.verify(buttonService).create(Material.METAL, Color.GREEN, 15); // count
+        assertEquals("Button\n" +
+                        "\tmaterial: Metal\n" +
+                        "\tcolor: Green\n" +
+                        "\tamount: 15",
+                lowerBodyClothes.getPart(0).toString()); // check result
+    }
+}
